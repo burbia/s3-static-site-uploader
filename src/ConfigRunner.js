69b39e5a1c80bc54c8f5f2b4c6b71f775197cad5
Angel Asenjo <asenjowork@gmail.com>
2017-12-14 11:25:26 +0100

    test

--- src/ConfigRunner.js
+++ src/ConfigRunner.js
@@ -47,7 +47,7 @@ return function ConfigRunner(){
                         break;
                     case 'upload':
                         fileUtils.getContents(obj.path).then(function(contents){
-                            console.log('uploading: ' + obj.path);
+                            console.log('1 uploading: ' + obj.path);
                             s3Wrapper.putObject(config.bucketName,obj.path,contents).then(function(){
                                 console.log('done uploading: ' + obj.path);
                             },function(reason){
